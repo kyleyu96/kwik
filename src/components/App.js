f4883d953452f350669f21efba669707a1bbfee1
Amir Bawab <amirbawab@gmail.com>
2019-01-20 01:49:49 -0500

    Updated theme

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,24 @@ import { withStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import HomeContainer from "../containers/HomeContainer";
 import logo from "../assets/logo.png";
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import ETA from "./ETA"
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#444444'
+        },
+        secondary: {
+            main: '#ffab40'
+        }
+      },
+});
 
 const styles = theme => ({
 	logo: {
@@ -24,29 +42,46 @@ const styles = theme => ({
       left: 0,
       zIndex: -1,
       opacity: 0.06
-    }
+    },
+    grow: {
+        flexGrow: 1,
+    },
+     menuButton: {
+        marginLeft: -12,
+        marginRight: 20,
+     },
 });
 
 class App extends Component {
 	render() {
 		const { classes } = this.props;
 		return (
-			<HashRouter>
-				<Fragment>
-            <img className={classes.kwik_bg} src={require('../assets/background.jpg')} />
-					<Grid
-						container
-						direction="column"
-						justify="flex-start"
-						alignItems="center"
-					>
-						<Grid item className={classes.grid}>
-							<img src={logo} className={classes.logo} />
-						</Grid>
-					</Grid>
-					<Route exact path="/" component={HomeContainer} />
-				</Fragment>
-			</HashRouter>
+            <div>
+                <img className={classes.kwik_bg} src={require('../assets/background.jpg')} />
+                <MuiThemeProvider theme={theme}>
+                    <HashRouter>
+                        <Fragment>
+                            <AppBar style={{ background: '#444444' }} position="static">
+                                <Toolbar>
+                                    <Typography variant="h6" color="inherit" className={classes.grow}>
+                                        <img src={logo} className={classes.logo} />
+                                  </Typography>
+                                  <Button color="inherit">Login</Button>
+                                </Toolbar>
+                              </AppBar>
+                            <Grid
+                                container
+                                direction="column"
+                                justify="flex-start"
+                                alignItems="center"
+                            >
+                            </Grid>
+                            <Route exact path="/" component={HomeContainer} />
+                            <Route exact path="/ETA" component={ETA} />
+                        </Fragment>
+                    </HashRouter>
+                </MuiThemeProvider>
+            </div>
 		);
 	}
 }

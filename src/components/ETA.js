34efd8cc7cb6ea4bf0e4d9b14f81faadaba28929
yjiang28 <lillyj970920@gmail.com>
2019-01-20 11:47:08 -0500

    restyle eta and waitconfirmation

--- src/components/ETA.js
+++ src/components/ETA.js
@@ -9,6 +9,7 @@ import FormControl from "@material-ui/core/FormControl";
 import FormLabel from "@material-ui/core/FormLabel";
 import TextField from "@material-ui/core/TextField";
 import { Link } from "react-router-dom";
+import Paper from "@material-ui/core/Paper";
 
 const styles = theme => ({
     eta_title: {
@@ -28,6 +29,16 @@ const styles = theme => ({
     },
     eta_button: {
         margin: theme.spacing.unit
+    },
+    container: {
+        marginTop: 50,
+        minWidth: 550
+    },
+    title: {
+        marginTop: 6 * theme.spacing.unit
+    },
+    button: {
+        margin: 2 * theme.spacing.unit
     }
 });
 
@@ -37,66 +48,80 @@ class ETA extends Component {
         var tip = (5 + Math.random() * 10).toFixed(2);
 
         return (
-            <Grid container spacing={16}>
-                <Grid item xs={12}>
-                    <Grid
-                        container
-                        className={classes.eta_title}
-                        justify="center"
-                        spacing={16}
-                    >
-                        <Typography variant="h4">
-                            Estimate Time Arrival
-                        </Typography>
-                    </Grid>
-                    <Grid
-                        container
-                        className={classes.eta_title}
-                        justify="center"
-                        spacing={16}
-                    >
-                        <Typography variant=" subtitle1">
-                            Let the customer know (on average) in how many hours
-                            they should expect their package
-                        </Typography>
-                    </Grid>
-                    <Grid
-                        container
-                        className={classes.eta_hours}
-                        justify="center"
-                        spacing={16}
-                    >
-                        <TextField
-                            label="Hours"
-                            type="number"
-                            defaultValue="2"
-                            className={classes.textField}
-                            InputLabelProps={{
-                                shrink: true
-                            }}
-                            margin="normal"
-                        />
-                    </Grid>
-                    <Grid
-                        container
-                        className={classes.eta_tip}
-                        justify="center"
-                        spacing={16}
-                    >
-                        <Typography variant="h5">Your Tip: CAD {tip}</Typography>
-                    </Grid>
-                    <Grid container justify="center" spacing={16}>
-                        <Link to={"/WaitConfirmation"}>
-                            <Button
-                                color="primary"
-                                variant="contained"
-                                className={classes.button}
+            <Grid
+                className={classes.container}
+                container
+                spacing={24}
+                direction="column"
+                alignItems="center"
+                justify="center"
+            >
+                <Paper className={classes.paper} style={{ minWidth: 550 }}>
+                    <Grid container spacing={16}>
+                        <Grid item xs={12}>
+                            <Grid
+                                container
+                                className={classes.eta_title}
+                                justify="center"
+                                spacing={16}
                             >
-                                Confirm
-                            </Button>
-                        </Link>
+                                <Typography variant="h4">
+                                    Estimate Time Arrival
+                                </Typography>
+                            </Grid>
+                            <Grid
+                                container
+                                className={classes.eta_title}
+                                justify="center"
+                                spacing={16}
+                            >
+                                <Typography variant=" subtitle1">
+                                    Let the customer know (on average) in how
+                                    many hours they should expect their package
+                                </Typography>
+                            </Grid>
+                            <Grid
+                                container
+                                className={classes.eta_hours}
+                                justify="center"
+                                spacing={16}
+                            >
+                                <TextField
+                                    label="Hours"
+                                    type="number"
+                                    defaultValue="2"
+                                    className={classes.textField}
+                                    InputLabelProps={{
+                                        shrink: true
+                                    }}
+                                    margin="normal"
+                                />
+                            </Grid>
+                            <Grid
+                                container
+                                className={classes.eta_tip}
+                                justify="center"
+                                spacing={16}
+                            >
+                                <Typography variant="h5">
+                                    Your Tip: CAD {tip}
+                                </Typography>
+                            </Grid>
+                            <Grid container justify="center" spacing={16}>
+                                <Link to={"/WaitConfirmation"}>
+                                    <Button
+                                        color="primary"
+                                        variant="contained"
+                                        className={classes.button}
+                                        fullWidth
+                                    >
+                                        Confirm
+                                    </Button>
+                                </Link>
+                            </Grid>
+                        </Grid>
                     </Grid>
-                </Grid>
+                </Paper>
             </Grid>
         );
     }
